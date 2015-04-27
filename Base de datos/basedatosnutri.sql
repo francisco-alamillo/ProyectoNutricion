1dd9dc8ba6802594beaf393696145f5066d1fd52
francisco-alamillo <francisco_alamillo93@hotmail.com>
2015-04-27 18:45:48 -0500

    base de datos perrona
    los adoro <3

--- Base de datos/basedatosnutri.sql
+++ Base de datos/basedatosnutri.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-04-2015 a las 23:17:55
+-- Tiempo de generación: 28-04-2015 a las 01:41:49
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -39,9 +39,9 @@ CREATE TABLE IF NOT EXISTS `datos_antropometricos` (
   `circunferencia_cintura` varchar(4) NOT NULL,
   `circunferencia_cadera` varchar(4) NOT NULL,
   `circunferencia_abdominal` varchar(4) NOT NULL,
-  `complexion` varchar(4) NOT NULL,
+  `Complexion` varchar(10) NOT NULL,
   `peso_teorico` varchar(4) NOT NULL,
-  `porentaje_pesoTeorico` varchar(4) NOT NULL,
+  `porcentaje_pesoTeorico` varchar(4) NOT NULL,
   `porcentaje_pesoHabitual` varchar(4) NOT NULL,
   `imc` varchar(4) NOT NULL,
   `peso_minimoMaximo` varchar(12) NOT NULL,
@@ -59,9 +59,121 @@ CREATE TABLE IF NOT EXISTS `datos_antropometricos` (
   `areaMuscularBrazo` varchar(5) NOT NULL,
   `areaMuscularTotal` varchar(5) NOT NULL,
   `aguaCorporalTotal` varchar(5) NOT NULL,
-  `peso/talla` varchar(5) NOT NULL,
-  `peso/edad` varchar(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `peso_talla` varchar(5) NOT NULL,
+  `peso_edad` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos_antropometricos_adolescente`
+--
+
+CREATE TABLE IF NOT EXISTS `datos_antropometricos_adolescente` (
+  `id_paciente` int(11) NOT NULL,
+  `peso_actual` varchar(3) NOT NULL,
+  `peso_habitual` varchar(3) NOT NULL,
+  `estatura` varchar(3) NOT NULL,
+  `pliegue_cutaneo_tricipital` varchar(3) NOT NULL,
+  `pliegue_cutaneo_bicipital` varchar(3) NOT NULL,
+  `pliegue_cutaneo_subescapular` varchar(3) NOT NULL,
+  `pliegue_cutaneo_suprailiaco` varchar(3) NOT NULL,
+  `circunferencia_de_brazo` varchar(3) NOT NULL,
+  `circunferencia_de_cintura` varchar(3) NOT NULL,
+  `circunferencia_de_cadera` varchar(3) NOT NULL,
+  `circunferencia_abdominal` varchar(3) NOT NULL,
+  `complexion` varchar(3) NOT NULL,
+  `peso_teorico` varchar(3) NOT NULL,
+  `porcentaje_peso_teorico` varchar(3) NOT NULL,
+  `porcentaje_peso_habitual` varchar(3) NOT NULL,
+  `indice_de_masa_corporal` varchar(3) NOT NULL,
+  `peso_minimo_y_maximo_recomendado_porIMC` varchar(3) NOT NULL,
+  `porcentaje_grasa_corporal` varchar(3) NOT NULL,
+  `grasa_corporal_total` varchar(3) NOT NULL,
+  `masa_libre_de_grasa` varchar(3) NOT NULL,
+  `porcentaje_exceso_o_deficiencia_de_grasa_corporal` varchar(3) NOT NULL,
+  `exceso_o_deficiencia_de_grasa_corporal` varchar(3) NOT NULL,
+  `pliegue_cutaneo_tricipital_mas_pliegue_cutaneo` varchar(3) NOT NULL,
+  `subescapular` varchar(3) NOT NULL,
+  `cutaneo_tricipital` varchar(3) NOT NULL,
+  `cutaneo_subescapular` varchar(3) NOT NULL,
+  `indice_cinturaCadera` varchar(3) NOT NULL,
+  `circunf_abdomen` varchar(3) NOT NULL,
+  `area_muscular_brazo` varchar(3) NOT NULL,
+  `masa_muscular_total` varchar(3) NOT NULL,
+  `agua_corporal_total` varchar(3) NOT NULL,
+  `peso_talla` varchar(3) NOT NULL,
+  `peso_edad` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos_antropometricos_deportista`
+--
+
+CREATE TABLE IF NOT EXISTS `datos_antropometricos_deportista` (
+  `id_paciente` int(11) NOT NULL,
+  `peso_actual` varchar(3) NOT NULL,
+  `peso_habitual` varchar(3) NOT NULL,
+  `estatura` varchar(3) NOT NULL,
+  `pliegue_cutaneo_tricipital` varchar(3) NOT NULL,
+  `pliegue_cutaneo_bicipital` varchar(3) NOT NULL,
+  `pliegue_cutaneo_subescapular` varchar(3) NOT NULL,
+  `pliegue_cutaneo_suprailiaco` varchar(3) NOT NULL,
+  `pliegue_cutaneo_de_pantorrilla` varchar(3) NOT NULL,
+  `circunferencia_brazo` varchar(3) NOT NULL,
+  `circunferencia_cintura` varchar(3) NOT NULL,
+  `circunferencia_cadera` varchar(3) NOT NULL,
+  `circunferencia_abdominal` varchar(3) NOT NULL,
+  `circunferencia_pantorrilla` varchar(3) NOT NULL,
+  `porcentaje_masa_grasa` varchar(3) NOT NULL,
+  `masa_osea` varchar(3) NOT NULL,
+  `masa_residual` varchar(3) NOT NULL,
+  `somatotipo` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos_antropometricos_mayores`
+--
+
+CREATE TABLE IF NOT EXISTS `datos_antropometricos_mayores` (
+  `id_paciente` int(11) NOT NULL,
+  `peso_actual` varchar(10) NOT NULL,
+  `estatura` varchar(10) NOT NULL,
+  `Pliegue_tricipital` varchar(11) NOT NULL,
+  `Pliegue_bicipital` varchar(11) NOT NULL,
+  `Pliegue_subescapular` varchar(11) NOT NULL,
+  `Pliegue_suprailiaco` varchar(11) NOT NULL,
+  `Circunferencia_de_brazo` varchar(11) NOT NULL,
+  `Circunferencia_de_cintura` varchar(11) NOT NULL,
+  `Circunferencia_de_cadera` varchar(11) NOT NULL,
+  `Circunferencia_de_abdominal` varchar(11) NOT NULL,
+  `Altura_de_rodilla` varchar(11) NOT NULL,
+  `Distancia_rodilla-maleolo` varchar(11) NOT NULL,
+  `Complexion` varchar(20) NOT NULL,
+  `peso_teorico` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `porcentaje_peso_teorico` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `porcentaje_peso_habitual` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `indice_de_masa_corporal` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `peso_minimo_y_maximo_recomendado_porIMC` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `porcentaje_grasa_corporal` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `grasa_corporal_total` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `masa_libre_de_grasa` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `porcentaje_exceso_o_deficiencia_grasa_corporal` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `exceso_o_deficiencia_grasa_corporal` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `pliegue_cutaneo_tricipital_mas_pliegue_cutaneo` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `subescapular` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `pliegue_cutaneo_tricipital` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `pliegue_cutaneo_subescapular` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `indice_cintura_cadera` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `circunferencia_abdominal` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `area_muscular_de_brazo` varchar(3) CHARACTER SET latin1 NOT NULL,
+  `masa_muscular_total` varchar(3) NOT NULL,
+  `agua_corporal_total` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -85,7 +197,7 @@ CREATE TABLE IF NOT EXISTS `datos_antropometricos_ninos` (
   `Peso/Talla` int(11) NOT NULL,
   `Peso/Edad` int(11) NOT NULL,
   `Pliegue tricipital` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -106,7 +218,7 @@ CREATE TABLE IF NOT EXISTS `datos_base` (
   `telefono` varchar(10) NOT NULL,
   `email` varchar(30) NOT NULL,
   `motivoConsulta` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -125,7 +237,7 @@ CREATE TABLE IF NOT EXISTS `estilo_vida` (
   `duracion` int(11) NOT NULL,
   `veces_x_semana` int(11) NOT NULL,
   `probemas_para_actividadFisica` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Índices para tablas volcadas
